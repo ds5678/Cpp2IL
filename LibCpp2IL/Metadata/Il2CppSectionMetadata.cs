df3df9ab67c29861f685fccab2732fb881ac96af
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-02-05 01:37:23 -0800

    Add documentation to Il2CppSectionMetadata

--- LibCpp2IL/Metadata/Il2CppSectionMetadata.cs
+++ LibCpp2IL/Metadata/Il2CppSectionMetadata.cs
@@ -4,8 +4,20 @@ public class Il2CppSectionMetadata : ReadableClass
 {
     // Prior to version 38, this class didn't exist; it was just a pair of ints.
 
+    /// <summary>
+    /// The byte offset of the section
+    /// </summary>
     public int Offset;
+    /// <summary>
+    /// The size of the section in bytes
+    /// </summary>
     public int Size;
+    /// <summary>
+    /// The number of entries in the section
+    /// </summary>
+    /// <remarks>
+    /// This is not present on versions before 38.
+    /// </remarks>
     [Version(Min = 38)] public int Count;
 
     public override void Read(ClassReadingBinaryReader reader)
